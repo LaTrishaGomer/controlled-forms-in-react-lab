8a3b4de7adbd11e97484f43a9f184bff3a37e825
latrisha <l_gomer@yahoo.com>
2025-04-02 22:22:35 -0700

    wip: finish lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Bookshelf from './components/Bookshelf/Bookshelf'; 
+import './app.css';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />  
+    </>
+  );
 };
 
 export default App;
